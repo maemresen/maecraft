eab8326529da1c59e98c2f72c3033827bc96e5d6
Emre Sen <maemresen@yazilim.vip>
2021-01-17 22:45:43 +0300

    I: divide into functions

--- doorOpen.lua
+++ doorOpen.lua
@@ -26,76 +26,87 @@ STATE_DOOR_OPEN = false
 --
 -- Functions
 --
-function findAcceptedPlayers()
+-- Display Helpers
+function dispScanDialog()
+	monitor.clear()
+	monitor.setCursorPos(1, 1)
+	monitor.write("Scan")
+	monitor.setCursorPos(1, 2)
+	monitor.write("Face...")
+	monitor.setCursorPos(1, 3)
+	monitor.write("Hold")
+	monitor.setCursorPos(1, 4)
+	monitor.write("On :)")
+end
+
+function dispSuccessDialog()
+	monitor.setCursorPos(1, 1)
+	monitor.write("Reis")
+	monitor.setCursorPos(1, 2)
+	monitor.write("Eve")
+	monitor.setCursorPos(1, 3)
+	monitor.write("Hos")
+	monitor.setCursorPos(1, 4)
+	monitor.write("Geldin")
+end
+
+function dispFailedDialog()
+	for i = 1, 3 do
+		local a = i
+		local b = 6 - i
+		monitor.setCursorPos(a + 1, a)
+		monitor.write("X")
+		monitor.setCursorPos(a + 1, b)
+		monitor.write("X")
+		monitor.setCursorPos(b + 1, a)
+		monitor.write("X")
+		monitor.setCursorPos(b + 1, b)
+		monitor.write("X")
+	end
+end
+
+function isObjectAllowed(object)
+	if not object["IsPlayer"] then
+		return false
+	end
+
+	local username = object["Username"]
+	local pos = object["Position"]
+	local x = pos["X"]
+	local y = pos["Y"]
+	local z = pos["Z"]
+	local inRange = (y == -1) and (-3 < x and x < 0) and (-4.5 < z and z < 0)
+	--print("x=",x, ", y=", y, ", z=", z)
+	return inRange and ACCEPTED_PLAYERS[username]
+end
+
+function scanObjects()
 	for name, value in pairs(proximity.getTargets()) do
-		if value["IsPlayer"] then
-			--
-			-- Check only players
-			--
-			local username = value["Username"]
-			local pos = value["Position"]
-			local x = pos["X"]
-			local y = pos["Y"]
-			local z = pos["Z"]
-			local inRange = (y == -1) and (-3 < x and x < 0) and (-4.5 < z and z < 0)
-			--print("x=",x, ", y=", y, ", z=", z)
-			if inRange then
-				if not STATE_DOOR_OPEN then
-					monitor.clear()
-					monitor.setCursorPos(1, 1)
-					monitor.write("Scan")
-					monitor.setCursorPos(1, 2)
-					monitor.write("Face...")
-					monitor.setCursorPos(1, 3)
-					monitor.write("Hold")
-					monitor.setCursorPos(1, 4)
-					monitor.write("On :)")
-					sleep(0.5)
-				end
-				if ACCEPTED_PLAYERS[username] then
-					return true
-				end
+		if isObjectAllowed(value) then
+			if not STATE_DOOR_OPEN then
+				dispScanDialog()
+				sleep(0.5)
 			end
+			STATE_DOOR_OPEN = true
+			return
 		end
 	end
-	return false
+	STATE_DOOR_OPEN = false
 end
 
-function loop()
-	while true do
-		--
-		-- Search if one of accepted player is in range of sensor
-		--
-		STATE_DOOR_OPEN = findAcceptedPlayers() or false
-		redstone.setOutput("back", STATE_DOOR_OPEN)
+while true do
+	--
+	-- Search if one of accepted player is in range of sensor
+	--
+	scanObjects()
+	redstone.setOutput("back", STATE_DOOR_OPEN)
 
-		monitor.clear()
-		if STATE_DOOR_OPEN then
-			monitor.setCursorPos(1, 1)
-			monitor.write("Reis")
-			monitor.setCursorPos(1, 2)
-			monitor.write("Eve")
-			monitor.setCursorPos(1, 3)
-			monitor.write("Hos")
-			monitor.setCursorPos(1, 4)
-			monitor.write("Geldin")
-		else
-			for i = 1, 3 do
-				local a = i
-				local b = 6 - i
-				monitor.setCursorPos(a + 1, a)
-				monitor.write("X")
-				monitor.setCursorPos(a + 1, b)
-				monitor.write("X")
-				monitor.setCursorPos(b + 1, a)
-				monitor.write("X")
-				monitor.setCursorPos(b + 1, b)
-				monitor.write("X")
-			end
-		end
-
-		sleep(0.2)
+	monitor.clear()
+	if STATE_DOOR_OPEN then
+		dispSuccessDialog()
+	else
+		dispFailedDialog()
 	end
-end
 
-loop()
+	sleep(0.2)
+end
