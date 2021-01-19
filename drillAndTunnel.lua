cb944e452e2007ba96f62571551476b1a6e78cc2
Emre Sen <maemresen@yazilim.vip>
2021-01-19 16:30:09 +0300

    I: drill and tunnel added

--- drillAndTunnel.lua
+++ drillAndTunnel.lua
@@ -1,34 +1,4 @@
-local depth = ...
+local depth, tunnelSize = ...
 
--- Constants
-local SLOT_FUEL = 1
-local SLOT_LADDER = 2
-local SLOT_BLOCK = 3
-
--- Functions
-function refuel()
-	if turtle.getFuelLevel() == 0 then
-		turtle.select(SLOT_FUEL)
-		turtle.refuel(1)
-	end
-end
-
--- Putting Ladders
-for i = 1, depth do
-	refuel()
-	turtle.digDown()
-	turtle.down()
-
-	-- put ladder
-	turtle.dig()
-	turtle.select(SLOT_LADDER)
-	turtle.place()
-end
-
--- Closing second drill
-for i = 1, depth do
-	refuel()
-	turtle.up()
-	turtle.select(SLOT_BLOCK)
-	turtle.placeDown()
-end
+shell.run("driller", depth)
+shell.run("tunnel", tunnelSize)
